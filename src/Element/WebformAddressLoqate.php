4cfccb080350b1f472151bd6c30d81c813dd4c36
rakeshjames <rakeshjames@1177822.no-reply.drupal.org>
2018-08-12 10:25:03 +0100

    Issue #2991954 by rakesh.gectcr: fatal error: Declaration of Drupal\loqate\Element\WebformAddressLoqate::getCompositeElements() must be compatible

--- src/Element/WebformAddressLoqate.php
+++ src/Element/WebformAddressLoqate.php
@@ -37,7 +37,7 @@ class WebformAddressLoqate extends WebformCompositeBase {
   /**
    * {@inheritdoc}
    */
-  public static function getCompositeElements() {
+  public static function getCompositeElements(array $elements) {
     $elements = [];
     
     $elements['address'] = [
