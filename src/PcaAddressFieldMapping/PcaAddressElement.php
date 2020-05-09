cbb0176bc93157ee1768bafb0ff77dc0a6d36b4d
Sang Lostrie <sang.lostrie@gmail.com>
2020-05-09 21:58:34 +0100

    Added install config and some more field mapping elements constants

--- src/PcaAddressFieldMapping/PcaAddressElement.php
+++ src/PcaAddressFieldMapping/PcaAddressElement.php
@@ -9,13 +9,21 @@ final class PcaAddressElement {
 
   public const ADDRESS_LOOKUP = 'address_lookup';
 
+  public const ADMINISTRATIVE_AREA = 'administrative_area';
+
+  public const LOCALITY = 'locality';
+
+  public const DEPENDENT_LOCALITY = 'dependent_locality';
+
+  public const POSTAL_CODE = 'postal_code';
+
+  public const SORTING_CODE = 'sorting_code';
+
   public const LINE1 = 'address_line1';
 
   public const LINE2 = 'address_line2';
 
-  public const LOCALITY = 'locality';
-
-  public const POSTAL_CODE = 'postal_code';
+  public const ORGANIZATION = 'organization';
 
   static function getConstants() {
     return (new \ReflectionClass(__CLASS__))->getConstants();
