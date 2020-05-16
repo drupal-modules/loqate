d90a8964565dd30a322191125243f7d4bc236215
Sang Lostrie <sang.lostrie@gmail.com>
2020-05-16 17:19:01 +0100

    Fixed wrong lookup logic in Loqate::getApiKey()

--- src/Loqate.php
+++ src/Loqate.php
@@ -14,20 +14,17 @@ final class Loqate {
   /**
    * Gets the Loqate API key value.
    *
-   * @param string $config_key
-   *   Optional custom config key.
+   * @param string $key_id
+   *   The key id to lookup.
    * @return string|null
    *   The key value if found or NULL.
    */
-  public static function getApiKey($config_key = LoqateApiKeyConfigForm::DEFAULT_API_KEY) {
-    $key_id = \Drupal::config('loqate.loqateapikeyconfig')->get($config_key);
-    if ($key_id) {
-      /** @var \Drupal\key\KeyRepositoryInterface $key_repository */
-      $key_repository = \Drupal::service('key.repository');
-      $key_entity = $key_repository->getKey($key_id);
-      if ($key_entity) {
-        return $key_entity->getKeyValue();
-      }
+  public static function getApiKey($key_id = LoqateApiKeyConfigForm::DEFAULT_API_KEY) {
+    /** @var \Drupal\key\KeyRepositoryInterface $key_repository */
+    $key_repository = \Drupal::service('key.repository');
+    $key_entity = $key_repository->getKey($key_id);
+    if ($key_entity) {
+      return $key_entity->getKeyValue();
     }
     return NULL;
   }
