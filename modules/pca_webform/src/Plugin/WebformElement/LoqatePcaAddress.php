5bfc1bd30bcb20b2dec45b25c849f7695acf8ecd
Sang Lostrie <sang.lostrie@gmail.com>
2020-05-27 22:03:05 +0100

    Added new pca_webform WebformElement

---
+++ modules/pca_webform/src/Plugin/WebformElement/LoqatePcaAddress.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\pca_webform\Plugin\WebformElement;
+
+use Drupal\loqate\PcaAddressFieldMapping\PcaAddressElement;
+use Drupal\webform\Plugin\WebformElement\WebformCompositeBase;
+
+/**
+ * Provides a 'PCA address' element.
+ *
+ * @WebformElement(
+ *   id = "pca_address",
+ *   label = @Translation("PCA address"),
+ *   description = @Translation("Provides a form element to collect address information (street, city, state, zip)."),
+ *   category = @Translation("Composite elements"),
+ *   multiline = TRUE,
+ *   composite = TRUE,
+ *   states_wrapper = TRUE,
+ *   dependencies = {
+ *     "loqate",
+ *   }
+ * )
+ *
+ * @see \Drupal\loqate\Element\PcaAddress
+ */
+class LoqatePcaAddress extends WebformCompositeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCompositeElements() {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @see \Drupal\loqate\Plugin\Field\FieldType\LoqatePcaAddressItem::schema
+   */
+  public function initializeCompositeElements(array &$element) {
+    $element['#webform_composite_elements'] = [
+      PcaAddressElement::LINE1 => [
+        '#title' => $this->t('Address Line 1'),
+        '#type' => 'textfield',
+        '#maxlength' => 255,
+      ],
+      PcaAddressElement::LINE2 => [
+        '#title' => $this->t('Address Line 2'),
+        '#type' => 'textfield',
+        '#maxlength' => 255,
+      ],
+      PcaAddressElement::LOCALITY => [
+        '#title' => $this->t('City/Town'),
+        '#type' => 'textfield',
+        '#maxlength' => 255,
+      ],
+      PcaAddressElement::ADMINISTRATIVE_AREA => [
+        '#title' => $this->t('State/Province'),
+        '#type' => 'textfield',
+        '#maxlength' => 255,
+      ],
+      PcaAddressElement::POSTAL_CODE => [
+        '#title' => $this->t('ZIP/Postal Code'),
+        '#type' => 'textfield',
+        '#maxlength' => 255,
+      ],
+      PcaAddressElement::COUNTRY_CODE => [
+        '#title' => $this->t('Country'),
+        '#type' => 'textfield',
+        '#maxlength' => 2,
+      ],
+    ];
+  }
+
+}
